337ee7a5f0b049718fb6b34b7932afb9632e4128
Jung Lee <jungsu9503@gmail.com>
2024-12-04 21:38:37 -0500

    fixed type error

--- app/api/reservations/[reservationId]/route.ts
+++ app/api/reservations/[reservationId]/route.ts
@@ -15,19 +15,31 @@ export const DELETE = async (
   if (!currentUser) {
     return NextResponse.error();
   }
+  try {
 
-  const { reservationId } = params;
+    const { reservationId } = params;
+  
+    if (!reservationId || typeof reservationId !== "string") {
+      return NextResponse.json(
+        { error: "Invalid Reservation ID" },
+        { status: 400 }
+      );
+    }
+  
+    const reservation = await prisma.reservation.deleteMany({
+      where: {
+        id: reservationId,
+        OR: [{ userId: currentUser.id }, { listing: { userId: currentUser.id } }],
+      },
+    });
 
-  if (!reservationId || typeof reservationId !== "string") {
-    return new Error("Invalid Reservation ID");
+    return NextResponse.json(reservation);
+
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 
-  const reservation = await prisma.reservation.deleteMany({
-    where: {
-      id: reservationId,
-      OR: [{ userId: currentUser.id }, { listing: { userId: currentUser.id } }],
-    },
-  });
-
-  return NextResponse.json(reservation);
 };
